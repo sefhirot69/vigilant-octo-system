76fe31bbc7f62a31291e7dd2834144d2e8ce771b
sefhirot69 <josalillo@gmail.com>
2023-09-16 12:33:47 +0200

    Feat: finish command

--- src/Shared/Infrastructure/Command/BaseCommand.php
+++ src/Shared/Infrastructure/Command/BaseCommand.php
@@ -8,11 +8,15 @@ use App\Shared\Domain\Bus\Query\Query;
 use App\Shared\Domain\Bus\Query\QueryBusInterface;
 use App\Shared\Domain\Bus\Query\QueryResponse;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BaseCommand extends Command
 {
+    protected static $defaultName = 'command:default';
+
     public function __construct(
         protected readonly QueryBusInterface $queryBus,
+        protected readonly SerializerInterface $serializer,
     ) {
         parent::__construct();
     }
@@ -20,5 +24,16 @@ class BaseCommand extends Command
     public function ask(Query $query): ?QueryResponse
     {
         return $this->queryBus->ask($query);
+    }
+
+    protected function deserialize(array $data, string $class): mixed
+    {
+        $content = json_encode($data, JSON_THROW_ON_ERROR);
+
+        return $this->serializer->deserialize(
+            $content,
+            $class,
+            'json'
+        );
     }
 }
